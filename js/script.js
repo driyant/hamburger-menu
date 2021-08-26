59d472848e4acde6936536a9b97c791a1b8c3b62
unknown <driyant.mail@gmail.com>
2021-08-26 20:06:46 +0700

    Add js function for menu toggle 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("Loaded!");
+/*
+  Start - This is for menu button 1 functionality
+*/
+
+// Select target element
+const menuBtn1 = document.querySelector(".menu__btn__1");
+
+
+// Callback function toggleMenu1
+const toggleMenu1 = () => {
+    menuBtn1.classList.toggle("open");
+}
+
+// Add event listener for menu button 1
+menuBtn1.addEventListener("click", toggleMenu1);
+
+
+/*
+  End - This is for menu button 1 functionality
+*/
