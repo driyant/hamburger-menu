93838f4214dca70dab6ac6987b3f0924d6e3832e
unknown <driyant.mail@gmail.com>
2021-08-26 21:24:06 +0700

    Styling menu button 3

--- js/script.js
+++ js/script.js
@@ -13,3 +13,29 @@ const toggleMenu1 = () => {
 
 // Add Event Listener For Menu Button 1
 menuBtn1.addEventListener("click", toggleMenu1);
+
+/*
+  End - This is for menu button 1 functionality
+*/
+
+
+/*
+  Start - This is for menu button 2 functionality
+*/
+
+// Select Target Element
+const menuBtn2 = document.querySelector(".menu__btn__2");
+
+
+// Callback Function toggleMenu2
+const toggleMenu2 = () => {
+    menuBtn2.classList.toggle("open");
+}
+
+// Add Event Listener For Menu Button 2
+menuBtn2.addEventListener("click", toggleMenu2);
+
+
+/*
+  End - This is for menu button 2 functionality
+*/
