5fc8565aed700ea8c54d129abd808a90b4861288
unknown <driyant.mail@gmail.com>
2021-08-26 21:09:59 +0700

    Add animation for menu button 2

--- js/script.js
+++ js/script.js
@@ -2,19 +2,14 @@
   Start - This is for menu button 1 functionality
 */
 
-// Select target element
+// Select Target Element
 const menuBtn1 = document.querySelector(".menu__btn__1");
 
 
-// Callback function toggleMenu1
+// Callback Function toggleMenu1
 const toggleMenu1 = () => {
     menuBtn1.classList.toggle("open");
 }
 
-// Add event listener for menu button 1
+// Add Event Listener For Menu Button 1
 menuBtn1.addEventListener("click", toggleMenu1);
-
-
-/*
-  End - This is for menu button 1 functionality
-*/
