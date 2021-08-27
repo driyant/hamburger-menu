b70959d2ca9116c09d1e484266ef60b692295e6f
unknown <driyant.mail@gmail.com>
2021-08-27 13:26:37 +0700

    Add functionality for menu button 5

--- js/script.js
+++ js/script.js
@@ -48,12 +48,12 @@ menuBtn2.addEventListener("click", toggleMenu2);
 const menuBtn3 = document.querySelector(".menu__btn__3");
 
 
-// Callback Function toggleMenu2
+// Callback Function toggleMenu3
 const toggleMenu3 = () => {
     menuBtn3.classList.toggle("open");
 }
 
-// Add Event Listener For Menu Button 2
+// Add Event Listener For Menu Button 3
 menuBtn3.addEventListener("click", toggleMenu3);
 
 
@@ -63,22 +63,43 @@ menuBtn3.addEventListener("click", toggleMenu3);
 
 
 /*
-  Start - This is for menu button 3 functionality
+  Start - This is for menu button 4 functionality
 */
 
 // Select Target Element
 const menuBtn4 = document.querySelector(".menu__btn__4");
 
 
-// Callback Function toggleMenu2
+// Callback Function toggleMenu4
 const toggleMenu4 = () => {
     menuBtn4.classList.toggle("open");
 }
 
-// Add Event Listener For Menu Button 2
+// Add Event Listener For Menu Button 4
 menuBtn4.addEventListener("click", toggleMenu4);
 
 
 /*
-  End - This is for menu button 3 functionality
+  End - This is for menu button 4 functionality
+*/
+
+/*
+  Start - This is for menu button 5 functionality
+*/
+
+// Select Target Element
+const menuBtn5 = document.querySelector(".menu__btn__5");
+
+
+// Callback Function toggleMenu4
+const toggleMenu5 = () => {
+    menuBtn5.classList.toggle("open");
+}
+
+// Add Event Listener For Menu Button 4
+menuBtn5.addEventListener("click", toggleMenu5);
+
+
+/*
+  End - This is for menu button 5 functionality
 */
